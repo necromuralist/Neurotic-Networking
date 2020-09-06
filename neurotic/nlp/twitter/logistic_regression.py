92b5f9e9aa1dccc94e542599d2ab9ea26cd57b1e
dogen <necromuralist@protonmail.com>
2020-09-05 18:14:25 -0700

    Added ellipse confidence intervals for the naive bayes model

---
+++ neurotic/nlp/twitter/logistic_regression.py
@@ -0,0 +1,20 @@
+# from pypi
+import attr
+import numpy
+
+# this project
+from .counter import WordCounter
+from .sentiment import TweetSentiment
+from .vectorizer import TweetVectorizer
+
+#
+#
+#    
+#
+#    
+#
+#    
+#
+#    
+#
+#
