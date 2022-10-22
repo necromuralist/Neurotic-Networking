8453c7f5ff31e79e37e358d01606b2d94d07c8db
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2022-10-22 16:13:36 -0700

    Changed the path to Neurotic-Networking to match the github name

--- dockerfiles/nvidia-pytorch-dockerfiles/run_image.fish
+++ dockerfiles/nvidia-pytorch-dockerfiles/run_image.fish
@@ -8,13 +8,18 @@
 
 # By changing this (initially commented-out) line to true
 # no-cgroups = true
+
+# this is the last part of the error I saw (the line is really long so I'm not putting it all in):
+# nvidia-container-cli: container error: cgroup subsystem devices not found: unknown.
+
+
 set PROJECTS $HOME/projects
 set NEUROTIC /home/neurotic
 
 docker run --gpus all --device /dev/nvidia0 --device /dev/nvidiactl \
        --device /dev/nvidia-modeset --device /dev/nvidia-uvm \
        -p 2222:22 --name neurotic-pytorch-conda \
-       --mount type=bind,source=$PROJECTS/neurotic-networks,target=$NEUROTIC/neurotic-networks \
+       --mount type=bind,source=$PROJECTS/Neurotic-Networking,target=$NEUROTIC/Neurotic-Networking \
        --mount type=bind,source=/media/data,target=$NEUROTIC/data \
        --mount type=bind,source=$PROJECTS/graeae,target=$NEUROTIC/graeae \
        --mount type=bind,source=$PROJECTS/models/,target=$NEUROTIC/models \
